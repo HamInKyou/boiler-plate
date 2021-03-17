44c41e4e043895a2e6d854041a8d588a5f9a89b5
HamInGyu <hfire1207@gmail.com>
2021-03-17 20:04:34 +0900

    feat: init redux setting

---
+++ client/src/_reducers/index.js
@@ -0,0 +1,14 @@
+import { combineReducers } from "redux";
+//import user from './user_reducer';
+
+//Store에는 Reducer들이 여러개 있을 수 있다.
+//왜? -> Reducer 안에서 하는 일이 어떻게 State가 변화하는지 보여준 다음에
+//변한 그 마지막 값을 리턴해주는게 Reducer인데,
+//State따라 Reducer가 나뉘어져 있다.
+//이 나뉘어진 Reducer들을 combineReducer라는 것을 이용해서
+//rootReducer에서 하나로 합쳐준다!
+const rootReducer = combineReducers({
+  //user
+});
+
+export default rootReducer;
--- client/src/index.js
+++ client/src/index.js
@@ -3,12 +3,34 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import { Provider } from "react-redux";
+import { applyMiddleware, createStore } from "redux";
+import promiseMiddleware from "redux-promise";
+import ReduxThunk from "redux-thunk";
+import Reducer from "./_reducers";
 
 import "antd/dist/antd.css";
 
+//그냥 store는 객체밖에 못 받기 때문에
+//promise와 function도 받기 위해서
+//redux-promise와 redux-thunk 미들웨어와 함께 만들어준다.
+const createStoreWithMiddleware = applyMiddleware(
+  promiseMiddleware,
+  ReduxThunk
+)(createStore);
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    {/*우리 앱에 리덕스 연결하기*/}
+    <Provider
+      store={createStoreWithMiddleware(
+        Reducer,
+        window.__REDUX_DEVTOOLS_EXTENSION__ &&
+          window.__REDUX_DEVTOOLS_EXTENSION__()
+      )}
+    >
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
